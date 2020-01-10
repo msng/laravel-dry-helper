94c02ab1f85de496f77d86ae4be1915031d8797e
msng <29099+msng@users.noreply.github.com>
2020-01-10 15:17:58 +0900

    Add Config/Database/Connection

---
+++ src/Constants/Config/Database/Connection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace msng\LaravelDryHelper\Constants\Config\Database;
+
+class Connection
+{
+    const SQLITE = 'sqlite';
+    const MYSQL = 'mysql';
+    const PGSQL = 'pgsql';
+    const SQLSRV = 'sqlsrv';
+}
