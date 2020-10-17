a453cff9760c4240f60d948d80047d097411c6df
msng <29099+msng@users.noreply.github.com>
2020-10-17 14:38:44 +0900

    Add OnUpdate

---
+++ src/Constants/Database/OnUpdate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace msng\LaravelDryHelper\Constants\Database;
+
+class OnUpdate
+{
+    const CASCADE = 'CASCADE';
+    const NO_ACTION = 'NO ACTION';
+    const RESTRICT = 'RESTRICT';
+    const SET_NULL = 'SET NULL';
+}
